1253d744cfa1c81e052d2afa4fa351857f586f10
shuma <s.fujita223.ep@gmail.com>
2024-06-22 21:04:49 +0900

    done

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Post Writer",
+  description: "ブログ投稿ができるwebアプリケーションです。",
+  url: "https://localhost:3000",
+  ogImage: "",
+  links: {
+    x: "https://x.com/ads_affi",
+    github: "https://github.com/",
+  },
+};
